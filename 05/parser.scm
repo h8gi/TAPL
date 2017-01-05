5f00a1d5de909aea1a6b73b89b5f7d79e7409e56
yagi <hiro.yagi0245@gmail.com>
2017-01-05 21:26:45 +0900

    add lexer and parser

--- 05/parser.scm
+++ 05/parser.scm
@@ -1,29 +1,79 @@
-(use prcc)
+(use lalr matchable)
 
-(define term
-  (<or>
-   (<#> (<and_> (<c> #\()
-		(lazy term)
-		(<c> #\)))
-	1)
-   (lazy app)
-   (lazy var)
-   (lazy lam)))
+(define (read-until predicate #!optional (port (current-input-port)))
+  (with-input-from-port port
+    (lambda ()
+      (let loop ([c (peek-char)]
+		 [acc ""])
+	(cond [(eof-object? c)
+	       (read-char) acc]
+	      [(predicate c) acc]
+	      [else
+	       (read-char)
+	       (loop (peek-char) (conc acc c))])))))
 
+(define (read-while predicate #!optional (port (current-input-port)))
+  (read-until (complement predicate) port))
 
-(define var
-  (<@> (<r> "[a-zA-Z][a-zA-Z0-9]*")
-       string->symbol))
+(define (read-number #!optional (port (current-input-port)))
+  (let ([int (read-while char-numeric? port)])
+    (cond [(char=? (peek-char port) #\.)
+	   ;; consume dot
+	   (read-char port)
+	   (string-append int "." (read-number port))]
+	  [else int])))
 
-(define lam
-  (<and_> (<s> "^")
-	  var
-	  (<s> ".")
-	  term))
+(define (read-var #!optional (port (current-input-port)))
+  (conc (read-char port)
+	(read-while (lambda (c)
+		      (char-set-contains? char-set:letter+digit c))
+		    port)))
 
-(define app
-  (<and_> term
-	  term))
+(define (port-line #!optional (port (current-input-port)))
+  (let-values (((line _) (port-position port)))
+    line))
+ 
+(define (port-column #!optional (port (current-input-port)))
+  (let-values (((_ column) (port-position port)))
+    column))
 
+(define (lexer #!optional (port (current-input-port)))
+  (with-input-from-port port
+    (lambda ()
+      (read-while char-whitespace?)
+      (let ([location (make-source-location "*stdin*"
+					    (port-line)
+					    (port-column) -1 -1)]
+	    [c (peek-char)])
+	(match c
+	  [#!eof (read-char) '*eoi*]
+	  [(? char-numeric? c)
+	   (make-lexical-token 'NUMBER location (string->number (read-number)))]
+	  [#\^ (read-char)
+	   (make-lexical-token 'LAMBDA location #f)]
+	  [#\. (read-char)
+	   (make-lexical-token 'DOT location #f)]
+	  [#\( (read-char)
+	   (make-lexical-token 'LPAREN location #f)]
+	  [#\) (read-char)
+	   (make-lexical-token 'RPAREN location #f)]
+	  [else
+	   (make-lexical-token 'VAR location (string->symbol (read-var)))])))))
 
+(define lambda-parser
+  (lalr-parser
+   ;; --- Options
+   (output: lambda-parser "lambda.yy.scm")
+   ;; output the LALR table to lambda.out
+   (out-table: "lambda.out")
+   ;; --- token definitions
+   (NUMBER LAMBDA DOT LPAREN RPAREN VAR)
+   ;; --- rules
+   (exp (exp term) : (list 'apply $1 $2)
+	(term) : $1)
+   (term (VAR)	  : $1
+	 (NUMBER) : $1
+	 (LAMBDA VAR DOT exp) : (list 'lambda (list $2) $4)
+	 (LPAREN exp RPAREN) : $2)))
 
+(include "lambda.yy.scm")
